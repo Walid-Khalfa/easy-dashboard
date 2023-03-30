b98219ac390ee377f0387d25ace9b1658f676caa
Walid <43506524+Khelfa@users.noreply.github.com>
2023-03-30 11:34:30 +0100

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,2 +1,5 @@
+// Import the "crudController" module from a local file
 const methods = require("./crudController");
+
+// Export the result of calling "crudController" with the argument "Product"
 module.exports = methods.crudController("Product");
