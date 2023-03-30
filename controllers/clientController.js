fac556547604b323da35ebdaf1db878759cedb4b
Walid <43506524+Khelfa@users.noreply.github.com>
2023-03-30 11:22:59 +0100

    Update clientController.js

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,2 +1,5 @@
+// Import the "crudController" module from a local file
 const methods = require("./crudController");
+
+// Export the result of calling "crudController" with the argument "Client"
 module.exports = methods.crudController("Client");
