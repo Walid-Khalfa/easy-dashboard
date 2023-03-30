7c19e6ff799c79475e810cd307768372b134fc89
Walid <43506524+Khelfa@users.noreply.github.com>
2023-03-30 12:08:35 +0100

    Update errorHandlers.js

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -1,10 +1,5 @@
-/*
-  Catch Errors Handler
 
-  With async/await, you need some way to catch errors
-  Instead of using try{} catch(e) {} in each controller, we wrap the function in
-  catchErrors(), catch any errors they throw, and pass it along to our express middleware with next()
-*/
+// Function for catching and handling errors in asynchronous functions
 
 exports.catchErrors = (fn) => {
   return function (req, res, next) {
