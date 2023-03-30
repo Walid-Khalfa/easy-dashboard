c8552dd41eb91c6414ebe3a7b396dc7b5bf5ebd2
Walid <43506524+Khelfa@users.noreply.github.com>
2023-03-30 11:25:10 +0100

    Update leadController.js

--- controllers/leadController.js
+++ controllers/leadController.js
@@ -1,2 +1,5 @@
+// Import the "crudController" module from a local file
 const methods = require("./crudController");
+
+// Export the result of calling "crudController" with the argument "Lead"
 module.exports = methods.crudController("Lead");
